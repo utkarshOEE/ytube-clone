502cc3add5c677a80ef197396c25a52712f18d69
utkarshOEE <utkarsh18.iitg@gmail.com>
2021-06-18 12:43:53 +0530

    Updates

---
+++ precache-manifest.e08f811b1af3cc2bbcdfc01be2f5e0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10ff3ef90172e0a1a5fdaab2fd13017",
+    "url": "/ytube-clone/index.html"
+  },
+  {
+    "revision": "6c40192697d5cc06444a",
+    "url": "/ytube-clone/static/css/main.6875b2c6.chunk.css"
+  },
+  {
+    "revision": "eff41b1986cf56a9a994",
+    "url": "/ytube-clone/static/js/2.74f157e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ytube-clone/static/js/2.74f157e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c40192697d5cc06444a",
+    "url": "/ytube-clone/static/js/main.73486291.chunk.js"
+  },
+  {
+    "revision": "65dcb07ec1cef81fe1b1",
+    "url": "/ytube-clone/static/js/runtime-main.5c706f27.js"
+  }
+]);
